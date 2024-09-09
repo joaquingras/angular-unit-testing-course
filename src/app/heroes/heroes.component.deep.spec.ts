a2f6f1f0892b05913a2ca3befa179a53599b73c3
Joaquín Gras <jgrasg@gmail.com>
2024-09-09 19:08:19 +0200

    ✅ Alternative method to trigger events

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -61,9 +61,10 @@ describe('HeroesComponent (deep tests)', () => {
     );
 
     heroComponentDEs.forEach((heroComponentDE, index) => {
-      heroComponentDE
-        .query(By.css('button'))
-        .triggerEventHandler('click', { stopPropagation: () => {} });
+      // heroComponentDE
+      //   .query(By.css('button'))
+      //   .triggerEventHandler('click', { stopPropagation: () => {} });
+      (<HeroComponent>heroComponentDE.componentInstance).delete.emit(null)
 
       expect(fixture.componentInstance.delete).toHaveBeenCalledWith(
         heroes[index]
